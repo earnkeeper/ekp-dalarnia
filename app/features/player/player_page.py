e94e807b8431c853bb0e69d0060b7a619a5319f1
gavin-shaw <gavin@gnarus.io>
2022-06-20 20:32:53 +0200

    feat: add description to player page

--- app/features/player/player_page.py
+++ app/features/player/player_page.py
@@ -1,5 +1,5 @@
 from ekp_sdk.ui import (Card, Col, Container, Div, Image, Row, Span, Tab, Tabs, Button,
-                        format_currency, format_template, Input, Avatar, Form, Link, format_mask_address)
+                        format_currency, format_template, Input, Avatar, Form, Link, format_mask_address, Paragraphs)
 from app.features.player.transactions_table import transactions_table
 from app.utils.page_title import page_title
 
@@ -9,12 +9,18 @@ def player_page(TRANSACTIONS_COLLECTION_NAME, SUMMARY_COLLECTION_NAME):
         children=[
             page_title('user', 'Player History', include_back=True),
             player_address(SUMMARY_COLLECTION_NAME),
-            Div(style={"marginBottom": "10px"}),
+            page_description(),
+            Div(style={"height": "28px"}),
             summary_row(SUMMARY_COLLECTION_NAME),
             transactions_table(TRANSACTIONS_COLLECTION_NAME)
         ]
     )
 
+def page_description():
+    return Paragraphs([
+        "We index the BSC chain to find all the Mines of Dalarnia transactions for this address, and calculate your total position.",
+        "This is a new deep dive, and might be missing transaction entries. Notice something off? Let us know on discord and we will fix it as soon as possible."
+    ])
 
 def player_address(SUMMARY_COLLECTION_NAME):
     return Div(
